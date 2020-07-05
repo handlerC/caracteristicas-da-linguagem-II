f9c63279614090b8f5b14f264f23eb0a1877ab4b
Haroldo Cuba <ha.shinji@gmail.com>
2020-07-04 23:41:26 -0300

    Peimeiro commit

---
+++ src/main/java/br/com/globallabs/exemplogradle/Exercicio.java
@@ -0,0 +1,8 @@
+package br.com.globallabs.exemplogradle;
+
+public class Exercicio {
+    public static void main(String[] args) {
+        var nome = "Andre";
+        System.out.println(nome);
+    }
+}
