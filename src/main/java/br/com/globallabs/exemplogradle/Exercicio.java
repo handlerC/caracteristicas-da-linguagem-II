b0e59682fd6f0dcb86af3cbc2dea58a47a752b18
Haroldo Cuba <ha.shinji@gmail.com>
2020-07-05 00:00:52 -0300

    execucao de exercicio

--- src/main/java/br/com/globallabs/exemplogradle/Exercicio.java
+++ src/main/java/br/com/globallabs/exemplogradle/Exercicio.java
@@ -4,5 +4,9 @@ public class Exercicio {
     public static void main(String[] args) {
         var nome = "Andre";
         System.out.println(nome);
+        System.out.println("A B C D E F G".toCharArray());
+        System.out.println("Aula de JAva".split(" "));
+        System.out.println("Aula".concat(" de JAva"));
+        System.out.println("1234 asda qw".replaceAll("[0-9]", "#"));
     }
 }
